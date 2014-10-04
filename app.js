bd6fa5a00b55a7168cc3af16224294bbfb96c303
Collin Stedman <cstedman@princeton>
2014-10-04 17:19:32 -0400

    Adding static content

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/test', function(req, res) {
+  res.sendfile('public/test.html');
 });
+
 app.listen(3737);
